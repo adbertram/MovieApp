d4832fe15e033a6b740df779e04649fa4be48084
Adam Bertram <adbertram@gmail.com>
2020-03-17 15:46:07 -0500

    t

--- infra_build_scripts/parse_arm_deployment_output.ps1
+++ infra_build_scripts/parse_arm_deployment_output.ps1
@@ -8,28 +8,32 @@ param (
     [switch]$MakeOutput
 )
 
-Write-Output "Retrieved input: $ArmOutputString"
-$armOutputObj = $ArmOutputString | convertfrom-json
+try {
+    Write-Output "Retrieved input: $ArmOutputString"
+    $armOutputObj = $ArmOutputString | convertfrom-json
 
-$armOutputObj.PSObject.Properties | ForEach-Object {
-    $type = ($_.value.type).ToLower()
-    $keyname = "Output_"+$_.name
-    $value = $_.value.value
+    $armOutputObj.PSObject.Properties | ForEach-Object {
+        $type = ($_.value.type).ToLower()
+        $keyname = "Output_"+$_.name
+        $value = $_.value.value
 
-    $varPrefix = "##vso[task.setvariable variable=$keyname"
+        $varPrefix = "##vso[task.setvariable variable=$keyname"
 
-    $vsoAttribs = @("task.setvariable variable = $_")
-    if ($type -eq "securestring") {
-        $vsoAttribs += 'isSecret = true'
-    } elseif ($type -ne "string") {
-        throw "Type '$type' is not supported for '$keyname'"
+        $vsoAttribs = @("task.setvariable variable = $_")
+        if ($type -eq "securestring") {
+            $vsoAttribs += 'isSecret = true'
+        } elseif ($type -ne "string") {
+            throw "Type '$type' is not supported for '$keyname'"
+        }
+
+        if ($MakeOutput.IsPresent) {
+            $vsoAttribs += 'isOutput = true'
+        }
+
+        $attribString = $vsoAttribs -join ';'
+        $var = "##vso[$attribString]$($value)"
+        Write-Output -InputObject $var
     }
-
-    if ($MakeOutput.IsPresent) {
-        $vsoAttribs += 'isOutput = true'
-    }
-
-    $attribString = $vsoAttribs -join ';'
-    $var = "##vso[$attribString]$($value)"
-    Write-Output -InputObject $var
+} catch {
+    Write-Error -Message $_.Exception.Message
 }
